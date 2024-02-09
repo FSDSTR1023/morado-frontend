5a00efe21b23b7f750f26d77a0412f3a0c5a4bef
22guerrabruno <139650855+22guerrabruno@users.noreply.github.com>
2024-02-09 12:01:06 +0100

    Ya se suben las imagenes

--- frontHotel/src/utils/HandelApiUsers.jsx
+++ frontHotel/src/utils/HandelApiUsers.jsx
@@ -1,19 +1,17 @@
 import axios from 'axios';
 
-const baseUrl = "http://localhost:5000/users"
-
-
+const baseUrl = 'http://localhost:6000/users';
 
 // ========:::: GET ALL  ::::========
 
 const getAllusers = async (setAllusers) => {
   try {
-   await axios.get(baseUrl).then(({ data }) => {
-      console.log("data --->", data);
+    await axios.get(baseUrl).then(({ data }) => {
+      console.log('data --->', data);
       setAllusers(data);
     });
   } catch (error) {
-    console.error("No se pueden obtener los usuarios");
+    console.error('No se pueden obtener los usuarios');
   }
 };
 
--- frontHotel/src/utils/HandleApiRooms.jsx
+++ frontHotel/src/utils/HandleApiRooms.jsx
@@ -1,19 +1,17 @@
 import axios from 'axios';
 
-const baseUrl = "http://localhost:5000/rooms"
-
-
+const baseUrl = 'http://localhost:6000/rooms';
 
 // ========:::: GET ALL  ::::========
 
 const getAllRooms = async (setAllRooms) => {
   try {
-   await axios.get(baseUrl).then(({ data }) => {
+    await axios.get(baseUrl).then(({ data }) => {
       // console.log("data --->", data);
       setAllRooms(data);
     });
   } catch (error) {
-    console.error("No se pueden obtener las Habitaciones");
+    console.error('No se pueden obtener las Habitaciones');
   }
 };
 
