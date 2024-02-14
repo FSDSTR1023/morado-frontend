280668e2fe003a7f17f4e0e45a3b1430623e26b9
FAverruz <146257059+FAverruz@users.noreply.github.com>
2024-02-15 00:18:17 +0100

    Update main.jsx

--- frontHotel/main.jsx
+++ frontHotel/main.jsx
@@ -1,16 +1,30 @@
-/* eslint-disable no-unused-vars */
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-// import App from "./src/App.jsx";
 import App from "./src/App.jsx";
 import "./src/index.css";
 import RoomProvider from "./src/context/RoomContext.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <RoomProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </RoomProvider>
 );
+
+// /* eslint-disable no-unused-vars */
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { BrowserRouter } from "react-router-dom";
+// // import App from "./src/App.jsx";
+// import App from "./src/App.jsx";
+// import "./src/index.css";
+// import RoomProvider from "./src/context/RoomContext.jsx";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <RoomProvider>
+//       <BrowserRouter>
+//         <App />
+//       </BrowserRouter>
+//   </RoomProvider>
+// );
