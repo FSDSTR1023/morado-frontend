2e0dae6f6e6fabd6f54f122cb100646316dded76
FAverruz <146257059+FAverruz@users.noreply.github.com>
2024-02-15 00:40:41 +0100

    Update main.jsx

--- frontHotel/main.jsx
+++ frontHotel/main.jsx
@@ -1,32 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './src/App.jsx';
-import './src/index.css';
-import RoomProvider from './src/context/RoomContext.jsx';
+/* eslint-disable no-unused-vars */
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./src/App.jsx";
+import "./src/index.css";
+import RoomProvider from "./src/context/RoomContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RoomProvider>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RoomProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </RoomProvider>
-  </React.StrictMode>
+  </RoomProvider>
 );
-
-// /* eslint-disable no-unused-vars */
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import { BrowserRouter } from "react-router-dom";
-// // import App from "./src/App.jsx";
-// import App from "./src/App.jsx";
-// import "./src/index.css";
-// import RoomProvider from "./src/context/RoomContext.jsx";
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <RoomProvider>
-//       <BrowserRouter>
-//         <App />
-//       </BrowserRouter>
-//   </RoomProvider>
-// );
